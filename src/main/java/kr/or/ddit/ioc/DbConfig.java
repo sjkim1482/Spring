b8ff43bb18dd8191959030e4b26afb905a4adeed
sjkim <monochrose94@gmail.com>
2021-01-29 10:51:20 +0900

    프레임워크와 라이브러리 스프링 프레임워크의 탄생 배경
    IOC 컨테이너
    
    IOC 컨테이너 생성
     - sts3 플러그인 설치
     - 스프링 설정파일(스프링 빈 생성) 작성
     - scope 테스트(singleton, prototype)
     - collection 객체 생성
     - property-placeholder
     - java config

---
+++ src/main/java/kr/or/ddit/ioc/DbConfig.java
@@ -0,0 +1,41 @@
+package kr.or.ddit.ioc;
+
+public class DbConfig {
+	
+	private String driverClassName;
+	private String url;
+	private String username;
+	private String password;
+	
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "DbConfig [driverClassName=" + driverClassName + ", url=" + url + ", username=" + username
+				+ ", password=" + password + "]";
+	}
+	
+}
---
+++ src/main/java/kr/or/ddit/user/model/UserVo.java
@@ -0,0 +1,32 @@
+package kr.or.ddit.user.model;
+
+public class UserVo {
+	private String userid;
+	private String usernm;
+	
+	public UserVo() {}
+	
+	public UserVo(String userid, String usernm) {
+		this.userid = userid;
+		this.usernm = usernm;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUsernm() {
+		return usernm;
+	}
+	public void setUsernm(String usernm) {
+		this.usernm = usernm;
+	}
+	@Override
+	public String toString() {
+		return "UserVo [userid=" + userid + ", usernm=" + usernm + "]";
+	}
+	
+	
+	
+}
